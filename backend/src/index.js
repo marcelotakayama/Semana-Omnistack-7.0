21c92491cd785f7984850877147c572cc2191471
marcelotakayama <marcelo.takayama00@gmail.com>
2019-10-18 00:44:35 -0300

    Início do projeto, já está conectado com o banco

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://semana:semana@clusterapi-zoq3q.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+});
+
+
+app.listen(3333);
